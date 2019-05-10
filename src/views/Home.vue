d53dfab4cebea94bf186566bc6b28c9373abc501
Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>
2019-05-10 09:39:30 +0200

    Signed-off-by: Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,15 @@
             <br/><br/>
             <h1>What is this?</h1>
             <p>Easy to re-use boilerplate to start implementing your favorite feature of your favorite blockchain in VueJS,
-                already preloaded vuex, vue-router and Beet login / logout. The UI framework built-in is bootstrap.</p>
+                already preloaded vuex, vue-router, bootstrap as frontend library and Beet login / logout functionality.</p>
 
             <h1>How does it work?</h1>
             <p>If you are trying this out online and want to implement your own app, please visit </p>
             <b-link href="https://github.com/beetapp/beet-vue-boilerplate">https://github.com/beetapp/beet-vue-boilerplate</b-link>
+
+            <h1>What can I do with Beet?</h1>
+            <p>Read up on Beet in the About page and checkout the examples shown in beet-js</p>
+            <b-link href="https://github.com/bitshares/beet-js/tree/master/examples">https://github.com/bitshares/beet-js/tree/master/examples</b-link>
         </div>
     </ContentCard>
 </template>
